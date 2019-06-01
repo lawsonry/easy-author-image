13121dc21a43b606542c44a41e0144709913ed38
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2019-06-01 16:10:45 +0530

    Add latest media popup

--- js/easy-author-image-uploader.js
+++ js/easy-author-image-uploader.js
@@ -1,35 +1,57 @@
-jQuery(document).ready(function($){
-	$('#author_profile_picture_button').click(function() {
-		//type = image,audio,video,file. If we write it wrong, nothing will appear. type = file by default
-			
-		//tb_show(caption, url, imageGroup)
-		// Google: 'ImageGroup tb_show thickbox':
-		//The optional imageGroup parameter can also be used to pass in an array of images for a single or multiple image slide show gallery.
-		// The problem is that inserting a gallery needs an associated post to work
-		tb_show('Upload an author profile picture! (Easy Author Image Plugin v1.5)', 'media-upload.php?referer=profile&amp;type=image&amp;TB_iframe=true&amp;post_id=0', false);
-		return false;
-	});
+( function( $ ) {
+	$( document ).ready( function(){
 	
-	window.send_to_editor = function(html) {
-		// html returns a link like this:
-		// <a href="{server_uploaded_image_url}"><img src="{server_uploaded_image_url}" alt="" title="" width="" height"" class="alignzone size-full wp-image-125" /></a>
-		var image_url = html.attr('src');		
-		//alert(html);
-		$('#author_profile_picture_url').val(image_url); // updates our hidden field that will update our author's meta when the form is saved
-		tb_remove();
-		$('#author_profile_picture_preview').html('<img src="'+image_url+'" style="max-width:100%; max-height:100%;" />');
-				
-		$('#submit_options_form').trigger('click');
-		$('#upload_success').text('Here is a preview of the profile picture you chose. To save it as your profile picture, scroll to the bottom of this page and click Update Profile.');
+		$( '#author_profile_picture_remove' ).on( 'click', function() {
+			$('#author_profile_picture_url').val('');
+			$( '.author_profile_picture_preview' ).attr( 'src', $( '.author_profile_picture_preview' ).data( 'preview' ) );
+			$( this ).hide();
+			$( '#upload_success' ).text( 'Picture removed, please now scroll to the bottom of this page and click Update Profile.' );
+		});
 		
-	}
-	
-	$('#author_profile_picture_remove').click(function(){
-		$('#author_profile_picture_url').val('');
-		$('#author_profile_picture_preview').remove();
-		$('#upload_success').text('Picture removed, please now scroll to the bottom of this page and click Update Profile.');
+		if( $( '#author_profile_picture_url' ).val() ) {
+			$( '#author_profile_picture_remove' ).show();
+		} else {
+			$( '#author_profile_picture_remove' ).hide();
+		}
+		$( '#author_profile_picture_button' ).on( 'click', function( event ) {
+	        var file_frame;
+
+		    event.preventDefault();
+
+		    // If the media frame already exists, reopen it.
+		    if ( file_frame ) {
+		    	file_frame.open();
+		      	return;
+		    }
+
+		    // Create the media frame.
+		    file_frame = wp.media.frames.file_frame = wp.media({
+		    	title: $( this ).data( 'popup-title' ),
+		      	button: {
+		        	text: $( this ).data( 'popup-button-title' ),
+		      	},
+		      	multiple: false
+		    });
+
+		    // When an image is selected, run a callback.
+		    file_frame.on( 'select', function() {
+		      	var attachment = file_frame.state().get('selection').first();
+
+		      	var thumburl = attachment.attributes.sizes.thumbnail;
+		      	if ( thumburl.url ) {
+					$( '#author_profile_picture_url' ).val( thumburl.url ); // updates our hidden field that will update our author's meta when the form is saved
+					$( '.author_profile_picture_preview' ).attr( 'src', thumburl.url );
+					$('#upload_success').text('Here is a preview of the profile picture you chose. To save it as your profile picture, scroll to the bottom of this page and click Update Profile.');
+				}
+				if( $( '#author_profile_picture_url' ).val() ) {
+					$( '#author_profile_picture_remove' ).show();
+				} else {
+					$( '#author_profile_picture_remove' ).hide();
+				}
+		    });
+
+		    // Finally, open the modal
+		    file_frame.open();
+		});
 	});
-	
-	
-	
 });
